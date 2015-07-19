6e7448c33e9cdcec8b7c5ef03a4db1722dfb9f43
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-19 09:23:22 +0800

    Refactor primary_key_checker::check()

--- frackdb/relation/primary_key_checker.cc
+++ frackdb/relation/primary_key_checker.cc
@@ -2,15 +2,27 @@
 #include "primary_key_checker.hh"
 
 Primary_key_checker::Primary_key_checker(const std::vector<std::string>& names, const std::vector<std::string>& primary_key)
-  : names(names), primary_key(primary_key) {
+  : names(names), primary_key(primary_key), names_iterator(names.begin()), primary_key_iterator(primary_key.begin()) {
 }
 
 // TODO: refactor
-void Primary_key_checker::check() const {
-  auto primary_key_iterator = primary_key.begin();
-  for (auto names_iterator = names.begin(); primary_key_iterator != primary_key.end(); ++names_iterator, ++primary_key_iterator) {
-    auto found = std::find(names_iterator, names.end(), *primary_key_iterator);
-    if (found == names.end() || names_iterator == names.end())
-      throw Primary_key_exception();
-  }
+void Primary_key_checker::check() {
+  while (primary_key_iterator != primary_key.end())
+    check_next_key_word_exists_in_names();
+}
+
+void Primary_key_checker::check_next_key_word_exists_in_names() {
+  auto found = std::find(names_iterator, names.end(), *primary_key_iterator);
+  check_key_word_exists_in_names(found);
+  advance_iterators();
+}
+
+void Primary_key_checker::check_key_word_exists_in_names(std::vector<std::string>::const_iterator iterator) {
+  if (iterator == names.end() || names_iterator == names.end())
+    throw Primary_key_exception();
+}
+
+void Primary_key_checker::advance_iterators() {
+  ++names_iterator;
+  ++primary_key_iterator;
 }
--- frackdb/relation/primary_key_checker.hh
+++ frackdb/relation/primary_key_checker.hh
@@ -8,7 +8,7 @@ public:
 
   Primary_key_checker(const std::vector<std::string>& names, const std::vector<std::string>& primary_key);
 
-  void check() const;
+  void check() ;
 
   DEFINE_EXCEPTION(Primary_key_exception, Attribute_list);
 
@@ -17,4 +17,10 @@ private:
   const std::vector<std::string>& names;
   const std::vector<std::string>& primary_key;
 
+  std::vector<std::string>::const_iterator names_iterator;
+  std::vector<std::string>::const_iterator primary_key_iterator;
+
+  void check_next_key_word_exists_in_names();
+  void check_key_word_exists_in_names(std::vector<std::string>::const_iterator iterator);
+  void advance_iterators();
 };
