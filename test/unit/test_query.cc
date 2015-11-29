04a391c91cb1141bb8370e05c3f47c991218c18b
Martin Fracker <martin.frackerjr@gmail.com>
2015-11-29 17:41:20 -0600

    Write test

--- test/unit/test_query.cc
+++ test/unit/test_query.cc
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 #include <querying/select.hh>
+#include <querying/project.hh>
+#include <relation/integer_attribute.hh>
 #include <relation/var_char_attribute.hh>
+#include <relation/row_checker.hh>
 #include <evaluation/comparison.hh>
 
 TEST(QueryTest, Select) {
@@ -15,4 +18,20 @@ TEST(QueryTest, Select) {
   const Row& row = selected_relation.get({ "martin" });
   EXPECT_EQ("martin", row.get_var_char("name"));
   EXPECT_EQ(1, selected_relation.size());
+}
+
+TEST(QueryTest, Project) {
+  Attribute_list attribute_list({ new Var_char_attribute("name", 15),
+                                  new Integer_attribute("age"),
+                                  new Var_char_attribute("gender", 6) }, { "name" });
+  Relation relation("people", attribute_list);
+  relation.add({ "martin", "21", "male" });
+
+  std::unique_ptr<Query> query(new Project({ "name", "age" }));
+  Relation projected_relation = query->execute(relation);
+  const Row& row = projected_relation.get({ "martin", "21" });
+
+  EXPECT_EQ("martin", row.get_var_char("name"));
+  EXPECT_THROW(row.get_int("age"), Row_checker::No_attribute_exception);
+  EXPECT_THROW(row.get_var_char("gender"), Row_checker::No_attribute_exception);
 }
