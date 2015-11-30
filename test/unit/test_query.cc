1fadd485223ee3ac26bcaf7db6c3face6172a8dd
Martin Fracker <martin.frackerjr@gmail.com>
2015-11-29 22:29:50 -0600

    Separated out test cases

--- test/unit/test_query.cc
+++ test/unit/test_query.cc
@@ -6,7 +6,7 @@
 #include <relation/row_checker.hh>
 #include <evaluation/comparison.hh>
 
-TEST(QueryTest, Select) {
+TEST(SelectTest, SelectsMatchedRows) {
   Attribute_list attribute_list({ new Var_char_attribute("name", 15) }, { "name" });
   Relation relation("people", attribute_list);
   relation.add({ "martin" });
@@ -20,7 +20,21 @@ TEST(QueryTest, Select) {
   EXPECT_EQ(1, selected_relation.size());
 }
 
-TEST(QueryTest, Project) {
+TEST(ProjectTest, ProjectionHasOnlyThoseChosenAttributes ) {
+  Attribute_list attribute_list({ new Var_char_attribute("name", 15),
+                                  new Integer_attribute("age") }, { "name" });
+  Relation relation("people", attribute_list);
+  relation.add({ "martin", "21" });
+
+  std::unique_ptr<Query> query(new Project({ "name" }));
+  Relation projected_relation = query->execute(relation);
+  const Row& row = projected_relation.get({ "martin" });
+
+  EXPECT_EQ("martin", row.get_var_char("name"));
+  EXPECT_THROW(row.get_int("age"), Row_checker::No_attribute_exception);
+}
+
+TEST(ProjectTest, ProjectionHasChosenAttributesAsItsKey) {
   Attribute_list attribute_list({ new Var_char_attribute("name", 15),
                                   new Integer_attribute("age"),
                                   new Var_char_attribute("gender", 6) }, { "name" });
@@ -29,9 +43,6 @@ TEST(QueryTest, Project) {
 
   std::unique_ptr<Query> query(new Project({ "name", "age" }));
   Relation projected_relation = query->execute(relation);
-  const Row& row = projected_relation.get({ "martin", "21" });
 
-  EXPECT_EQ("martin", row.get_var_char("name"));
-  EXPECT_THROW(row.get_int("age"), Row_checker::No_attribute_exception);
-  EXPECT_THROW(row.get_var_char("gender"), Row_checker::No_attribute_exception);
+  const Row& row = projected_relation.get({ "martin", "21" });
 }
