bd48aebe24809d20fda5bbb1f6046b2684f1429b
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-11 13:13:16 -0600

    Add copyright claim

--- frackdb/querying/query.hh
+++ frackdb/querying/query.hh
@@ -1,3 +1,11 @@
+//  Copyright 2016 Martin Fracker, Jr.
+//  All Rights Reserved.
+// 
+//  FrackDB is free software, released under the terms
+//  of the GNU General Public License v3. Please see the
+//  file LICENSE in the root directory or visit
+//  www.gnu.org/licenses/gpl-3.0.en.html for license terms.
+
 #pragma once
 
 #include <relation/relation.hh>
